484ae90fc6dd24ae0e1b7b8b8507621fabd119e5
user <user@WIN-HIJ4FH2GJF8>
2024-09-05 22:08:25 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EnumAsIs
+{
+    enum orientation : byte
+        {
+            north = 1,
+            south = 2,
+            east = 3,
+            west = 4,
+            northwest = 5,
+            northeast = 6,
+            northwestwest = 7
+        }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            byte directionByte;
+            string? directionString;
+            orientation myDirection = orientation.north;
+            Console.WriteLine($"myDirection = {myDirection}");
+            directionByte = (byte)myDirection;
+            directionString = Convert.ToString(myDirection);
+            Console.WriteLine($"byte equivalent = {directionByte}");
+            Console.WriteLine($"string equivalent = {directionString}");
+            Console.ReadKey();
+        }
+    }
+}
